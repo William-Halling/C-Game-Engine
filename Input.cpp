b57e0693a1466a596ab29ec50d527174bdf2e418
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-04-01 21:39:40 +0800

    Update Input.cpp

--- Input.cpp
+++ Input.cpp
@@ -3,31 +3,27 @@
 
 Input::Input()
 {
-    // Could register callbacks with glfwSetKeyCallback, etc.
-    // For simplicity we'll poll in Update().
+    // ...
 }
 
 
 void Input::Update()
 {
-      // Save previous states
     m_prevKeyStates = m_keyStates;
+    m_prevMouseStates = m_mouseStates;
 
-
-    // Poll keyboard (example: all keys up to GLFW_KEY_LAST)
+    // Poll keyboard
     for (int key = GLFW_KEY_SPACE; key <= GLFW_KEY_LAST; ++key)
     {
         bool pressed = (glfwGetKey(glfwGetCurrentContext(), key) == GLFW_PRESS);
         m_keyStates[key] = pressed;
     }
 
-
     // Poll mouse buttons
     for (int btn = GLFW_MOUSE_BUTTON_1; btn <= GLFW_MOUSE_BUTTON_LAST; ++btn)
     {
         bool pressed = (glfwGetMouseButton(glfwGetCurrentContext(), btn) == GLFW_PRESS);
-        m_keyStates[btn] = pressed; // store in same map with negative indices? Better separate.
-        
+        m_mouseStates[btn] = pressed;
     }
 
     glfwGetCursorPos(glfwGetCurrentContext(), &m_mouseX, &m_mouseY);
@@ -40,10 +36,34 @@ bool Input::IsKeyPressed(int key) const
     return it != m_keyStates.end() && it->second;
 }
 
+
 bool Input::IsKeyJustPressed(int key) const
 {
     auto itCurr = m_keyStates.find(key);
     auto itPrev = m_prevKeyStates.find(key);
     return (itCurr != m_keyStates.end() && itCurr->second) &&
            (itPrev == m_prevKeyStates.end() || !itPrev->second);
+}
+
+
+bool Input::IsMouseButtonPressed(int button) const
+{
+    auto it = m_mouseStates.find(button);
+    return it != m_mouseStates.end() && it->second;
+}
+
+
+bool Input::IsMouseButtonJustPressed(int button) const
+{
+    auto itCurr = m_mouseStates.find(button);
+    auto itPrev = m_prevMouseStates.find(button);
+    return (itCurr != m_mouseStates.end() && itCurr->second) &&
+           (itPrev == m_prevMouseStates.end() || !itPrev->second);
+}
+
+
+void Input::GetMousePosition(double& x, double& y) const
+{
+    x = m_mouseX;
+    y = m_mouseY;
 }
