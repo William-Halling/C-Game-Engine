71ac42305108cef036f1e09aea999a8b21e9921f
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-04-01 21:38:04 +0800

    Create Mesh.cpp

---
+++ Mesh.cpp
@@ -0,0 +1,43 @@
+#include "Mesh.h"
+#include <glad/glad.h>
+
+Mesh::Mesh(const std::vector<float>& vertices, const std::vector<unsigned int>& indices)
+    : m_indexCount(static_cast<unsigned int>(indices.size()))
+{
+    glGenVertexArrays(1, &m_VAO);
+    glGenBuffers(1, &m_VBO);
+    glGenBuffers(1, &m_EBO);
+
+    glBindVertexArray(m_VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+    // Position attribute (location = 0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+
+Mesh::~Mesh()
+{
+    glDeleteVertexArrays(1, &m_VAO);
+    glDeleteBuffers(1, &m_VBO);
+    glDeleteBuffers(1, &m_EBO);
+}
+
+
+void Mesh::Draw(Shader& shader)
+{
+    shader.Use();
+    glBindVertexArray(m_VAO);
+    glDrawElements(GL_TRIANGLES, m_indexCount, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
